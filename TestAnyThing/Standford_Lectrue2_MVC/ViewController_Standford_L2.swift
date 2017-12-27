458d7f0c120cdb8942d91dbb35cda44fd654bc62
AdamWD <falco1016@gmail.com>
2017-12-28 04:01:24 +0800

    Back up after lecture 4

--- TestAnyThing/Standford_Lectrue2_MVC/ViewController_Standford_L2.swift
+++ TestAnyThing/Standford_Lectrue2_MVC/ViewController_Standford_L2.swift
@@ -20,11 +20,27 @@ class ViewController_Standford_L2: UIViewController {
 
     private(set) var flipCount: Int = 0 {
         didSet {
-            flipCountLabel.text = "Flips: \(flipCount)"
+            updateFlipCountLabel()
         }
     }
     
+    
+    private func updateFlipCountLabel(){
+        let attributes: [NSAttributedStringKey:Any] = [
+            .strokeWidth: 5.0,
+            .strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        ]
+        
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
+    
     @IBOutlet private weak var flipCountLabel: UILabel!
+    {
+        didSet{
+            updateFlipCountLabel()
+        }
+    }
     
     @IBOutlet private var cardButtons: [UIButton]!
     
@@ -59,18 +75,19 @@ class ViewController_Standford_L2: UIViewController {
         }
     }
     
-    private var emojiChoices = ["🦇","😱", "🙀", "👿", "🎃", "👻", "🍭", "🍬","🍎"]
+//    private var emojiChoices = ["🦇","😱", "🙀", "👿", "🎃", "👻", "🍭", "🍬","🍎"]
+    private var emojiChoices = "🦇😱🙀👿🎃👻🍭🍬🍎"
     
-    private var emoji = [Int:String]()
+    private var emoji = [Card:String]()
 
     private func emoji(for card: Card) ->String
     {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
         
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
 
     func flipCard(withEmoji emoji: String, on button: UIButton)
